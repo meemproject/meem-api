23e09dbf60d017310ed2c47df245a936eb9c4185
Ken Goldfarb <hello@kengoldfarb.com>
2021-12-08 14:36:22 -0500

    feat: contract abis / types for Meem id and auctions; stubbed out tweet/hashtag model relationship

--- src/types/models.ts
+++ src/types/models.ts
@@ -1,9 +1,13 @@
+import Hashtag from '../models/Hashtag'
 import Meem from '../models/Meem'
 import Tweet from '../models/Tweet'
+import TweetHashtag from '../models/TweetHashtag'
 
 export interface IModels {
+	Hashtag: typeof Hashtag
 	Meem: typeof Meem
 	Tweet: typeof Tweet
+	TweetHashtag: typeof TweetHashtag
 }
 
 export type AppModel = IModels[keyof IModels]
