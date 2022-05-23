4d8b9ce18c9eda2a6e55004cc5a95d5de50b5f23
Brandon Minch <brandonminch@gmail.com>
2022-05-23 10:04:35 -0600

    Remove Meem type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,11 +2,9 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { ERC721 } from "./ERC721";
-export type { Meem } from "./Meem";
 export type { MeemID } from "./MeemID";
 export type { MeemMarket } from "./MeemMarket";
 
 export { ERC721__factory } from "./factories/ERC721__factory";
-export { Meem__factory } from "./factories/Meem__factory";
 export { MeemID__factory } from "./factories/MeemID__factory";
 export { MeemMarket__factory } from "./factories/MeemMarket__factory";
