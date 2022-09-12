df212c9cce6fe300ddc7830a1ed8786ba2131921
Brandon Minch <brandonminch@gmail.com>
2022-09-12 14:39:42 -0600

    Roles stuff

--- src/controllers/MeemContractController.ts
+++ src/controllers/MeemContractController.ts
@@ -530,4 +530,59 @@ export default class MeemContractController {
 			status: 'success'
 		})
 	}
+
+	public static async getMeemContractRoles(
+		req: IRequest<MeemAPI.v1.GetMeemContractRoles.IDefinition>,
+		res: IResponse<MeemAPI.v1.GetMeemContractRoles.IResponseBody>
+	): Promise<Response> {
+		if (!req.wallet) {
+			throw new Error('USER_NOT_LOGGED_IN')
+		}
+
+		if (!req.body.name) {
+			throw new Error('MISSING_PARAMETERS')
+		}
+
+		if (!req.body.metadata) {
+			throw new Error('MISSING_PARAMETERS')
+		}
+
+		await req.wallet.enforceTXLimit()
+
+		if (config.DISABLE_ASYNC_MINTING) {
+			try {
+				await services.meemContract.createMeemContract({
+					...req.body,
+					senderWalletAddress: req.wallet.address
+				})
+			} catch (e) {
+				log.crit(e)
+				sockets?.emitError(
+					config.errors.CONTRACT_CREATION_FAILED,
+					req.wallet.address
+				)
+			}
+		} else {
+			const lambda = new AWS.Lambda({
+				accessKeyId: config.APP_AWS_ACCESS_KEY_ID,
+				secretAccessKey: config.APP_AWS_SECRET_ACCESS_KEY,
+				region: 'us-east-1'
+			})
+
+			await lambda
+				.invoke({
+					InvocationType: 'Event',
+					FunctionName: config.LAMBDA_CREATE_CONTRACT_FUNCTION,
+					Payload: JSON.stringify({
+						...req.body,
+						senderWalletAddress: req.wallet.address
+					})
+				})
+				.promise()
+		}
+
+		return res.json({
+			status: 'success'
+		})
+	}
 }
---
+++ src/types/shared/api/getMeemContractRoles.shared.ts
@@ -0,0 +1,31 @@
+import { IError, HttpMethod, IApiResponseBody } from '../api.shared'
+
+export namespace GetMeemContractRoles {
+	export interface IPathParams {
+		/** The MeemContract id to fetch roles of */
+		meemContractId: string
+	}
+
+	export const path = (options: IPathParams) =>
+		`/api/1.0/meemContracts/${options.meemContractId}/roles`
+
+	export const method = HttpMethod.Get
+
+	export interface IQueryParams {}
+
+	export interface IRequestBody {}
+
+	export interface IResponseBody extends IApiResponseBody {
+		/** The MeemId */
+		roles: any[]
+	}
+
+	export interface IDefinition {
+		pathParams: IPathParams
+		queryParams: IQueryParams
+		requestBody: IRequestBody
+		responseBody: IResponseBody
+	}
+
+	export type Response = IResponseBody | IError
+}
