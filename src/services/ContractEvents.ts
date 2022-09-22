f078a795f3d16dd2ca0467a76e96bbb8232fa45e
Brandon Minch <brandonminch@gmail.com>
2022-09-22 09:57:47 -0600

    Remove meemcontract guild creation from contract events

--- src/services/ContractEvents.ts
+++ src/services/ContractEvents.ts
@@ -394,16 +394,6 @@ export default class ContractEvent {
 
 		await t.commit()
 
-		const meemContractGuild = await services.guild.getMeemContractGuild({
-			meemContractId: theMeemContract.id
-		})
-
-		if (!meemContractGuild) {
-			await services.guild.createMeemContractGuild({
-				meemContractId: theMeemContract.id
-			})
-		}
-
 		// Update ENS
 		await services.meemId.updateENS(theMeemContract)
 
