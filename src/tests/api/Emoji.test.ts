6d14e4c83fad2835384aad407380150f671dc338
Ken Goldfarb <hello@kengoldfarb.com>
2023-04-12 09:39:45 -0400

    emoji test

--- src/tests/api/Emoji.test.ts
+++ src/tests/api/Emoji.test.ts
@@ -1,5 +1,6 @@
 import { assert } from 'chai'
 import BaseTest from '../BaseTest'
+import emoji from './emoji.json'
 
 class EmojiTests extends BaseTest {
 	protected async setup() {
@@ -8,6 +9,18 @@ class EmojiTests extends BaseTest {
 
 	private async translateUnicode() {
 		assert.isOk(services.rule.unicodeToEmoji('2709-fe0f'))
+		Object.keys(emoji).forEach(category => {
+			const categoryEmojis = emoji[category]
+			categoryEmojis.forEach(e => {
+				const u = services.rule.unicodeToEmoji(e.u)
+				assert.isOk(u)
+				const emUnicode = services.rule.emojiToUnicode(u)
+				assert.isOk(emUnicode)
+				const [baseCode] = e.u.split('-')
+				assert.equal(parseInt(emUnicode, 16), parseInt(baseCode, 16))
+				console.log({ u, emUnicode })
+			})
+		})
 	}
 }
 
